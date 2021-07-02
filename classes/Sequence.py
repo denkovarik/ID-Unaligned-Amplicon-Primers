f192b08d48aa0cdc4d85a70a4df9bfe4576ebaa0
Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-07-01 23:34:36 -0600

    Implemented the Sequence class

--- classes/Sequence.py
+++ classes/Sequence.py
@@ -6,10 +6,35 @@
 # Description:
 #   This class represents a sequence of nucleotides.
 ###############################################################################
-
+import os, sys, inspect
+# Get the current and parent folder that files are located in
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+from classes.Nucleotide import Nucleotide
 
 class Sequence():
     """
     Class that is used to represent a Sequence.
     """
-    pass
+    def __init__(self, seq):
+        """
+        Initializes an instance of the Sequence class.
+
+        :param self: An instance of the Sequence class.
+        :param seq: A sequence of nucleotides as a list of Nucleotides.
+        """
+        if type(seq) == type("string"):
+            self.sequence = []
+            for c in seq:
+                self.sequence.append(Nucleotide(c))
+        # See if seq is a list
+        elif type(seq) == type([]):
+            ref = Nucleotide("A")
+            for nuc in seq:
+                if type(nuc) != type(ref):
+                    raise Exception("Invalid sequence of nucleotides for Sequence class.")
+            self.sequence = seq
+        else:
+            raise Exception("Invalid sequence of nucleotides for Sequence class.")
+        
