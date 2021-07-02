97eae0ac99d032b358509d359c763d0bdd0f60e9
Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-07-01 23:55:02 -0600

    Implemented ability to run script from any directory

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -2,30 +2,34 @@
 
 # This file runs all testing scripts for the project
 
+# Create filepaths
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
 # Run the tests for the Nucleotide class
 echo "Running Tests for Nucleotide Class"
-python Nucleotide_tests.py
+python $DIR"/Nucleotide_tests.py"
 
 # Run the tests for the Primer class
 echo "Running Tests for Primer Class"
-python Primer_tests.py
+python $DIR"/Primer_tests.py"
 
 # Run the tests for the Forward_Primer class
 echo "Running Tests for Forward_Primer Class"
-python Forward_Primer_tests.py
+python $DIR"/Forward_Primer_tests.py"
 
 # Run the tests for the Reverse_Primer class
 echo "Running Tests for Reverse_Primer Class"
-python Reverse_Primer_tests.py
+python $DIR"/Reverse_Primer_tests.py"
 
 # Run the tests for the Sequence class
 echo "Running Tests for Sequence Class"
-python Sequence_tests.py
+python $DIR"/Sequence_tests.py"
 
 # Run the tests for the Amplicon class
 echo "Running Tests for Amplicon Class"
-python Amplicon_tests.py
+python $DIR"/Amplicon_tests.py"
 
 # Run the tests for the Unaligned_Amplicons class
 echo "Running Tests for Unaligned_Amplicons Class"
-python Unaligned_Amplicons_tests.py
+python $DIR"/Unaligned_Amplicons_tests.py"
