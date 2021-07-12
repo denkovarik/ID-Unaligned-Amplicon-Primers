a734cb7efb45f013e6fb23924e0d6dd7ce69901d
Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-07-11 20:45:08 -0600

    Implemented class to represent a sequence

--- classes/Sequence.py
+++ classes/Sequence.py
@@ -23,7 +23,10 @@ class Sequence():
         This includes identifying which symbols are valid nucleotides whether
         to symbols for nucleotides are compliments of each other.
         """
+        # Set to indicated the valid nucleotide symbols
         valid_symbols = set(("A","C","G","T","U","a","c","g","t","u"))
+        # Dictionary to identify nucleotide complements. Should be faster than
+        # if statements
         compl = {
                     "A" : set(("t","T","u","U")),
                     "C" : set(("g","G")),
@@ -36,6 +39,31 @@ class Sequence():
                     "t" : set(("a","A")), 
                     "u" : set(("a","A")),
                 }
+        # Dictionary used for constructing complimentary sequences
+        get_compl =     {
+                            "A" : "T",
+                            "a" : "t",
+                            "T" : "A",
+                            "t" : "a",
+                            "G" : "C",
+                            "g" : "c",
+                            "C" : "G",
+                            "c" : "g",
+                        }
+        # Dictionary for identifying the number of hydrogen bonds between a 
+        # given nucleotide and its compliment
+        h_bonds =   {
+                        "A" : 2,
+                        "a" : 2,
+                        "T" : 2,
+                        "t" : 2,
+                        "U" : 2,
+                        "u" : 2,
+                        "G" : 3,
+                        "g" : 3,
+                        "C" : 3,
+                        "c" : 3,
+                    }
         pass
 
 
@@ -59,6 +87,7 @@ class Sequence():
 
         :param self: An instance of the Nucleotide class.
         :param other: An other nucleotide being compared
+        :return: Bool indicating if this seq is equal to other
         """
         return str(self) == str(other)
 
@@ -69,6 +98,7 @@ class Sequence():
 
         :param self: An instance of the Sequence class
         :param index: The index of the sequence to access
+        :return: The corresponding base at 'index'
         """
         if index < 0 or index >= len(self.sequence):
             raise Exception("Index is out of bounds")
@@ -80,6 +110,7 @@ class Sequence():
         Overloaded hash function for the Sequence class.
 
         :param self: An instance of the Sequence class
+        :return: The has for the class instance
         """
         return hash(self.sequence)
 
@@ -89,6 +120,7 @@ class Sequence():
         Overloaded function for the sequence length for the Sequence class.
 
         :param self: An instance of the Sequence class
+        :return: The length of the sequence as an int
         """
         return len(self.sequence)
 
@@ -99,14 +131,58 @@ class Sequence():
 
         :param self: An instance of the Sequence class.
         :param other: Another instance of the Sequence class to compare to.
+        :return: Boolean indicating whether this sequence is less than the 
+                 other.
         """
         return self.sequence < other.sequence
+
+
+    def primer_binary_srch(self, primers):
+        """
+        Searches a list of forward primers for one that binds to this instance 
+        of the sequence class. The search is completed via binary search, and 
+        a fP that binds to a search is one in which every base in the forward 
+        primer matches the corresponding base in the sequence.
+
+        :param self: An instance of the Sequence class
+        :param primers: A list of primers to match to
+        :return: The Forward_Primer that binds to the sequence
+        """
+        l = 0
+        r = len(primers) - 1
+        while l <= r:
+            m = l + int((r - l) / 2) 
+            if primers[m].binds_to(self):
+                return primers[m]
+            elif primers[m] < self:
+                l = m + 1
+            else:
+                r = m - 1
+        return None
+
+
+    def primer_srch(self, primers):
+        """
+        Searches a list of forward primers for one that binds to this instance 
+        of the sequence class. The search is completed by brute force, and a fP 
+        that binds to a search is one in which every base in the forward primer 
+        matches the corresponding base in the sequence.
+
+        :param self: An instance of the Sequence class
+        :param primers: A list of primers to match to
+        :return: The Forward_Primer that binds to the sequence
+        """
+        for fP in primers:
+            if fP.binds_to(self):
+                return fP
+        
 
     
     def __str__(self):
         """
-        Overloaded function to convert the instance of the class to a string..
+        Overloaded function to convert the instance of the class to a string.
 
         :param self: An instance of the Sequence class
+        :return: The string representation of the class.
         """
         return self.sequence
