43c16f0765058f12e356dcdaf63d85cb2458d16d
Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-07-11 20:46:58 -0600

    Included script to install and uninstall dependencies

---
+++ setup/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script install dependiences for the project
+
+# Install pysam to read .bam files
+pip install pysam
+# Install xlrd to read excel sheets with pandas
+pip install xlrd
+# Install pandas
+pip install pandas
+# Install progress bar
+pip install progress tqdm
---
+++ setup/uninstall_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script uninstall the dependencies for the project.
+
+# Uninstall pysam
+pip uninstall pysam
+# Uninstall xlrd
+pip uninstall xlrd
+# Uninstall pandas
+pip uninstall pandas
+# Uninstall progress bar
+pip uninstall progress tqdm
