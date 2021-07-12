8715efd79b76607a21d8c9c4b7d43975b883cdfc
Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-07-11 20:47:16 -0600

    Moved filie

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script install dependiences for the project
-
-# Install pysam to read .bam files
-pip install pysam
-# Install xlrd to read excel sheets with pandas
-pip install xlrd
